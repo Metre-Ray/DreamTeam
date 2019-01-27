7d051125d936617581d95192f4cc073448d675c9
Metre-Ray <dgary@tut.by>
2019-01-27 13:57:54 +0300

    feat: add map, some styling, pickout separate components from general file

--- src/pages/author_page.js
+++ src/pages/author_page.js
@@ -4,123 +4,32 @@ import authorObject from '../data/filmmakers.json';
 import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
-import ModalVideo from 'react-modal-video';
-import ReactPlayer from 'react-player';
-import { Player } from 'video-react';
 import './author_page.css';
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
-import L from 'leaflet';
-
-// pk.eyJ1IjoibWV0cmVyYXkiLCJhIjoiY2pyZG10ejJtMGg2dTQ5cWtuOWtsbWs4ZiJ9.vPmo1y2-ocH20LSe77hbZg
+import OverlayVideo from '../components/overlayVideo';
+import Mapp from '../components/Map';
 
 
-class MyModalVideo extends React.Component {
 
-    constructor () {
-        super()
-        this.state = {
-        isOpen: false
-        }
-        this.openModal = this.openModal.bind(this)
-    }
-
-    openModal () {
-        this.setState({isOpen: true})
-    }
-
-    render () {
-        return (
-        <div>
-            <ModalVideo channel='youtube' isOpen={this.state.isOpen} src="https://www.youtube.com/" videoId='' onClose={() => this.setState({isOpen: false})} />
-            <button onClick={this.openModal}>Open</button>
-        </div>
-        )
-    }
-}
-
-
-class ModalV extends React.Component {
-
+class AuthorPage extends React.Component {
     constructor(props) {
         super(props);
-        this.name = "Виктор Тимофеевич Туров"; //sessionStorage.getItem('filmmakerName');
-        this.lang = "rus";
-        this.state = {
-            isOpen: false
-        };
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-    }
-
-    openModal () {
-        this.setState({isOpen: true})
-    }
-
-    closeModal() {
-        this.setState({isOpen: false});
-    }
-
-
-    render () {
-        const modal = <div className="modal-window" onClick={this.closeModal}>
-                <div className="modal-content">
-                    <ReactPlayer url={authorObject[this.lang][this.name].link_to_video} />
-                </div>
-            </div>;
-        if (this.state.isOpen === true)
-            return (
-                <Fragment>
-                    {modal}
-                    <button onClick={this.openModal}>Open</button>
-                </Fragment>
-            );
-        else
-            return (
-                <Fragment>
-                    <button onClick={this.openModal}>Open</button>
-                </Fragment>
-            )
-    }
-}
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.name = "Виктор Тимофеевич Туров"; //sessionStorage.getItem('filmmakerName');
-        this.lang = "rus";
-    }
-
-    // componentDidMount() {
-    //     const mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    //     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //         maxZoom: 14,
-    //         id: 'mapbox.streets',
-    //         accessToken: 'pk.eyJ1IjoibWV0cmVyYXkiLCJhIjoiY2pyZG10ejJtMGg2dTQ5cWtuOWtsbWs4ZiJ9.vPmo1y2-ocH20LSe77hbZg'
-    //     }).addTo(mymap);
-    //     document.querySelector('#mapid').style.display = 'block';
-    //     // mymap.invalidateSize();
-    // }
-
-    async defineGeoCoords(place) {
-        const provider = new OpenStreetMapProvider();
-        const results = await provider.search({ query: 'Minsk, Belarus' });
-        console.log(results);
-        return results[0];
+        this.name = "Аслюк"; //sessionStorage.getItem('filmmakerName');
+        this.lang = "rus";  //sessionStorage.getItem('language');
     }
 
     render() {
         const imgsrc = `../img/${authorObject[this.lang][this.name].photo}`;
         const images = [];
-        this.defineGeoCoords();
         return (
             <Fragment>
-                <h2>{this.name}</h2>
-                <p><img src={imgsrc} alt="filmmaker's photo"></img></p>
+                <section className="top">
+                    <h2>{authorObject[this.lang][this.name].name}</h2>
+                    <p><img src={imgsrc} alt="filmmaker's photo"></img></p>
+                </section>
                 <section className="time-line">
                     <Timeline lineColor={'#ddd'}>
-                        {
-                            Object.keys(authorObject[this.lang][this.name].biography).map((date) => 
+                        {   
+                            Object.keys(authorObject[this.lang][this.name].biography).sort().map((date) => 
                                 <TimelineItem
                                     key={date}
                                     dateText={date}
@@ -152,20 +61,15 @@ class App extends React.Component {
                     <ImageGallery items={images} />
                 </section>
                 <section className="video">
-                    <ModalV />
-                    {/* <Player
-                        playsInline
-                        poster="/assets/poster.png"
-                        src={authorObject[this.lang][this.name].link_to_video}
-                    /> */}
+                    <OverlayVideo url={authorObject[this.lang][this.name].link_to_video} />
                 </section>
-                <section className="mapp">
-                    <div id="mapid"></div>
-                    MAAAAAAAAAP
+                <section className="map">
+                    <p>Место работы автора: </p>
+                    <Mapp place={authorObject[this.lang][this.name].place_of_working} />
                 </section>
             </Fragment>
         );
     }
 }
 
-ReactDOM.render(<App />, document.querySelector('main'));
+ReactDOM.render(<AuthorPage />, document.querySelector('main'));
