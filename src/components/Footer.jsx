6b093d296ac8f128af37f7a9c4314ef4dead8714
NastyaLo <loyko2802@gmail.com>
2019-01-28 00:31:55 +0300

    feat: add rss logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,13 @@ class Footer extends React.Component {
         return (
             <footer>
                 <div className="container">
-                    <div className="logo">Team35</div>
+                        <div class="logo">Team35</div>
+                <div class="footer-text">
+                    <p>All rights are right. All lefts are left.</p>
+                </div>
+                <div >
+                    <img class="rss-logo" src="https://rollingscopes.com/images/logo_rs_text.svg" alt="RSS"/>
+                </div>
                 </div>
             </footer>
         );
