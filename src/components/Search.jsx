b771d2dd572c24275dca3d3093c4b39f54c397b4
Metre-Ray <dgary@tut.by>
2019-01-30 19:50:09 +0300

    feat: get language from session storage for Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import '../css/searchPage.css';
 
 class Search extends React.Component {
   render () {
+    const lang = sessionStorage.getItem('language');
     return (
       <div className="wrapper">
           <div className="asking">
               <input type="search" name="" placeholder='поиск' className="input"/>
-              <input id="submit" type="submit" name="" value={sessionStorage.getItem('language') === 'eng' ? 'SEARCH!' : 'ПОИСК!'} className="search"/>
+              <input id="submit" type="submit" name="" value='ПОИСК!' className="search"/>
           </div>
           <div className="answer">
               <ul></ul>
