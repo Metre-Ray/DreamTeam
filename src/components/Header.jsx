2fa06f2b35b8acd26f8df98993a1d4287637fbd7
Metre-Ray <dgary@tut.by>
2019-01-30 19:44:55 +0300

    refractor: move choice of language into header and search into nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import changeLanguage from '../js/changeLanguage';
 
 const UIText = require('../data/user-interface.json');
 
 class Header extends React.Component {
+
+    componentDidMount() {
+        changeLanguage();    
+    }
+
     render () {
-        let lang = 'rus';
+        const lang = sessionStorage.getItem('language') || 'rus';
 
         return (
             <header>
                 <div className="container">
                     <div className="logo">Team35</div>
-                    <h1>Belarusian filmmakers</h1>
-                    <a href="./search.html" className="search">{UIText[lang]['search-button']}</a>
+                    <h1>{UIText[lang]['title']}</h1>
+                    {/* <a href="./search.html" className="search">{UIText[lang]['search-button']}</a> */}
+                    <ul className="chooseLang submenu">
+                        <li id="eng" key="1">eng</li>
+                        <li id="rus" key="2">rus</li>
+                        <li id="bel" key="3">bel</li>
+                    </ul>
                 </div>
             </header>
         );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,16 @@ const UIText = require('../data/user-interface.json');
 
 class Nav extends React.Component {
     render () {
-        let lang = sessionStorage.getItem('language') || 'rus';
+        const lang = sessionStorage.getItem('language') || 'rus';
 
         return(
             <nav>
                 <ul>
                     <li><a href="./index.html" className={"home-button" + this.props.isHomeActive}>{UIText[lang]['home-button']}</a></li>
-                    <li><a href="./authors/authors.html" className={"filmmakers-button" + this.props.isFilmmakersActive}>{UIText[lang]['filmmakers-button']}</a></li>
-                    <li><a href="" className={"language-button" + this.props.isLanguageActive}>{UIText[lang]['language-button']}</a>
+                    <li>
+                        <a href="./authors/authors.html" className={"filmmakers-button" + this.props.isFilmmakersActive}>{UIText[lang]['filmmakers-button']}</a>
                     </li>
-                    <li className="chooseLang">
-                        <ul className="submenu">
-                            <li id="eng" key="1">eng</li>
-                            <li id="rus" key="2">rus</li>
-                            <li id="bel" key="3">bel</li>
-                        </ul>
-                    </li>
+                    <li><a href="./search.html" className="search">{UIText[lang]['search-button']}</a></li>
                 </ul>
             </nav>
         );
