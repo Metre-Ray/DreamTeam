961ec7f42c445b720e2dde544a441eb212be20c1
Metre-Ray <dgary@tut.by>
2019-01-30 19:40:20 +0300

    refractor: refractor of code

--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,6 @@
-var textPlaseholder = '';
-var textButton = '';
-var submit = document.getElementById('submit');
-
-console.log(submit);
+let textPlaseholder = '';
+let textButton = '';
+const submit = document.getElementById('submit');
 
 switch (sessionStorage.getItem('language')) {
     case "rus":
@@ -24,67 +22,63 @@ switch (sessionStorage.getItem('language')) {
 } 
 
 window.onload = function (){
-    var el = document.querySelector('.input');
+    const el = document.querySelector('.input');
+    const button = document.querySelector('.search');
     el.placeholder=textPlaseholder;
-    var button = document.querySelector('.search');
     button.value = textButton;
     submit.click();
 };
 
 function search() {
-    var ul = document.querySelector("ul");
-    var searchfield = document.querySelector('.input').value;
-    console.log(sessionStorage.getItem('language'));
-    // var myExp = new RegExp(searchfield, 'i'); //любое совпадение
-    var myExp = new RegExp('^'+searchfield, 'i'); // совпадение по первым буквам
-    var xhttp = new XMLHttpRequest();
+    const searchfield = document.querySelector('.input').value;
+    // let myExp = new RegExp(searchfield, 'i'); //любое совпадение
+    const myExp = new RegExp('^' + searchfield, 'i'); // совпадение по первым буквам
+    const xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
 
         if (this.readyState == 4 && this.status == 200) {
 
-           if(sessionStorage.getItem('language') == undefined) //проверка языка страницы
-            {
+            const response = JSON.parse(xhttp.responseText);
+            const output = '';
+            const lang = sessionStorage.getItem('language');
+
+            if(lang == undefined) {
                 sessionStorage.setItem('language','rus');
-
+                lang = 'rus';
             }
 
-        var response = JSON.parse(xhttp.responseText);
-        var output = '';
-        var lang = sessionStorage.getItem('language');
+            const obj = response[lang];
+            const matchs = [];
 
-        var obj = response[lang];
-        var matchs =[];
-
-        for(key in obj){
-            if((key.search(myExp) != -1) || (obj[key].place_of_working.search(myExp) != -1)){
-                matchs.push(key);
+            for(key in obj){
+                if((key.search(myExp) !== -1) || (obj[key].place_of_working.search(myExp) !== -1)){
+                    matchs.push(key);
+                }
             }
-        }
-        matchs.forEach((el)=>{output +='<li><a href=\'https://metre-ray.github.io/DreamTeam/html/authors/'+ el +'.html\'>'+ obj[el].name +'</a></li>'}); 
-        
-        if(matchs.length == 0){
-            switch (sessionStorage.getItem('language')) {
-                case "rus":
-                    output = '<li>Совпадений не найдено =(</li>';
-                    break;
-                case "eng":
-                    output = '<li>No match found =(</li>';
-                    break;
-                case "by":
-                    output = '<li>Супадзенняў не знойдзена =(</li>';
-                    break;
-                default:
-                    output = '<li>Совпадений не найдено =(</li>';
-                    break;
-              } 
+            matchs.forEach((el)=>{output +='<li><a href=\'https://metre-ray.github.io/DreamTeam/html/authors/'+ el +'.html\'>'+ obj[el].name +'</a></li>'}); 
             
+            if(matchs.length === 0){
+                switch (lang) {
+                    case "rus":
+                        output = '<li>Совпадений не найдено =(</li>';
+                        break;
+                    case "eng":
+                        output = '<li>No match found =(</li>';
+                        break;
+                    case "by":
+                        output = '<li>Супадзенняў не знойдзена =(</li>';
+                        break;
+                    default:
+                        output = '<li>Совпадений не найдено =(</li>';
+                        break;
+                } 
+            }
+            document.querySelector("ul").innerHTML = output;        
         }
-        document.querySelector("ul").innerHTML = output;        
-    }
-};
-xhttp.open("GET", "../data/filmmakers.json", true);
-xhttp.send();
+    };
+    xhttp.open("GET", "../data/filmmakers.json", true);
+    xhttp.send();
 }
 
 submit.addEventListener('click', search, false);
