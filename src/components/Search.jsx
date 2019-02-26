382edecacbf2762e30025df6f25a109a5969b9af
garnich <garnich@tut.by>
2019-02-26 16:20:08 +0300

    fix: multilanguage search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import '../css/searchPage.css';
 
 class Search extends React.Component {
-  render () {
+  render() {
     const lang = sessionStorage.getItem('language');
     return (
       <div className="wrapper">
-          <div className="asking">
-              <input type="search" name="" placeholder='поиск' className="input"/>
-              <input id="submit" type="submit" name="" value='ПОИСК!' className="search"/>
-          </div>
-          <p>*search only in english</p>
-          <div className="answer">
-              <ul></ul>
-          </div>
+        <div className="asking">
+          <input type="search" name="" placeholder="поиск" className="input" />
+          <input id="submit" type="submit" name="" value="ПОИСК!" className="search" />
+        </div>
+        <div className="answer">
+          <ul />
+        </div>
       </div>
     );
   }
